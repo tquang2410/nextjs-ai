094f3bb83d3845470c1802b2c9f09a34b1f78a7e
tquang2410 <justicepencil@gmail.com>
2025-10-14 17:11:00 +0700

    Update route handler for dynamic prompt

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -6,10 +6,20 @@ import {createGoogleGenerativeAI} from "@ai-sdk/google";
 const google = createGoogleGenerativeAI({
     apiKey: process.env.GEMINI_API_KEY
 });
-export async function POST(){
+export async function POST(req: Request){
+try {
+    const {prompt} = await req.json();
     const {text} = await generateText({
+        // model: google('models/gemini-2.5-pro'),
         model: google('models/gemini-2.5-flash'),
-        prompt: "Write a poem about the sea in Vietnamese",
+        // prompt: prompt,
+        // Rút gọn cú pháp nếu tên biến trùng với tên thuộc tính
+        prompt,
+
     });
     return Response.json({text});
+} catch (error) {
+    console.error("Error generating text", error);
+    return Response.json({error: "Error generating text"}, {status: 500});
+}
 }
