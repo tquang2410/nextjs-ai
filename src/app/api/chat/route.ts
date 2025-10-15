36a8b25f348703ce4b8f6ae8776c705d1f3493ba
tquang2410 <justicepencil@gmail.com>
2025-10-15 19:54:46 +0700

    Add log for token tracking

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -5,9 +5,20 @@ export async function POST(req: Request){
         const {messages}: { messages: UIMessage[] } = await req.json();
         const result = streamText({
                 model: google('models/gemini-2.5-flash'),
+            // Chuyển đổi định dạng tin nhắn từ UI sang định dạng mà mô hình yêu cầu
                 messages: convertToModelMessages(messages)
             }
         );
+        // Track số token đã sử dụng
+        result.usage.then((usage) => {
+            console.log({
+                messagesCount: messages.length,
+                inputTokens: usage.inputTokens,
+                outputTokens: usage.outputTokens,
+                totalTokens: usage.totalTokens,
+            })
+        })
+        // Chuyển đổi kết quả sang định dạng phản hồi phù hợp với UI
         return result.toUIMessageStreamResponse();
     } catch (error) {
         console.error("Error streaming text:", error);
